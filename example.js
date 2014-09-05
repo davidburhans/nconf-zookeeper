0dd532fa01de938a53a20569257ca5013cfa6357
David Burhans <david.burhans@corvisa.com>
2014-09-05 12:35:56 -0600

    Created example and added it to README

---
+++ example.js
@@ -0,0 +1,29 @@
+var nconf = require('nconf');
+require(/* 'nconf-zookeeper' */'./');
+
+// tell nconf to use ZooKeeper
+nconf.use('zookeeper', {
+	host:'localhost', // ZooKeeper host name
+	port: '2181', // ZooKeeper port
+	rootPath: '/', // Root path for storing nconf file in ZooKeeper
+	name: 'nconf-zookeeper', // name of file stored in ZooKeeper at `rootPath`
+	autoSave: -1, // number of milliseconds to wait before autosaving changes to ZooKeeper
+				  // this allows batching updates. disable by setting to less than 0.
+				  // Setting to 0 results in immediate saves. Be careful.
+	autoUpdate: false // set to true to add a watcher to rootPath/name and auto reload the data when it is changed
+});
+
+// nconf will not load data from an async source without a `load` being requested
+nconf.load(function(storageData) {
+	// nconf is now initialized with the data contained in 'storageData' from ZooKeeper
+});
+
+nconf.set('example:test', 3);
+var example = nconf.get('example');
+
+console.log(example.test); // 3
+
+// nconf will not automatically persist data to ZooKeeper unless autoSave is enabled in nconf-zookeeper
+nconf.save(function() {
+	// data has now been persisted to ZooKeeper.
+});
